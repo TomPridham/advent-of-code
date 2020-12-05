02ba463b7ecbe470a3b45224da63e3de17cd209f
Tom Pridham <pridham.tom@gmail.com>
2020-12-05 14:14:21 -0700

    day 2 2020 complete

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,7 +1,11 @@
 mod day1;
+mod day2;
 use day1::{fix_expense_report, fix_expense_report_triple};
+use day2::{find_valid_passwords, find_valid_passwords_positional};
+
 fn main() {
     assert_eq!(731731, fix_expense_report());
     assert_eq!(116115990, fix_expense_report_triple());
-    print!("{}", fix_expense_report_triple())
+    assert_eq!(536, find_valid_passwords());
+    print!("{}", find_valid_passwords_positional())
 }
