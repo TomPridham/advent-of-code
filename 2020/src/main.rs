06129011fa255de62280296f0074076a0b87180f
Tom Pridham <pridham.tom@gmail.com>
2020-12-11 09:34:50 -0700

    2020 day 4 completed

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,9 +1,11 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 use day1::{fix_expense_report, fix_expense_report_triple};
 use day2::{find_valid_passwords, find_valid_passwords_positional};
 use day3::{tree_collision_detector, tree_collision_detector_v2};
+use day4::{strict_validate_passports, validate_passports};
 
 fn main() {
     assert_eq!(731731, fix_expense_report());
@@ -12,6 +14,8 @@ fn main() {
     assert_eq!(558, find_valid_passwords_positional());
     assert_eq!(145, tree_collision_detector());
     assert_eq!(3424528800, tree_collision_detector_v2());
+    assert_eq!(228, validate_passports());
+    assert_eq!(175, strict_validate_passports());
 
-    println!("{}", tree_collision_detector_v2())
+    println!("{}", strict_validate_passports())
 }
