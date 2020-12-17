860d1c23f23417b7787dc9807aaf0b43d44184fb
Tom Pridham <pridham.tom@gmail.com>
2020-12-16 22:32:21 -0700

    2020 day 6 complete

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -3,11 +3,13 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 use day1::{fix_expense_report, fix_expense_report_triple};
 use day2::{find_valid_passwords, find_valid_passwords_positional};
 use day3::{tree_collision_detector, tree_collision_detector_v2};
 use day4::{strict_validate_passports, validate_passports};
 use day5::{find_highest_seat, find_my_seat};
+use day6::{collate_customs_answers, collate_customs_answers_correctly};
 
 fn main() {
     assert_eq!(731731, fix_expense_report());
@@ -20,6 +22,8 @@ fn main() {
     assert_eq!(175, strict_validate_passports());
     assert_eq!(801.0, find_highest_seat());
     assert_eq!(597.0, find_my_seat());
+    assert_eq!(6504, collate_customs_answers());
+    assert_eq!(3351, collate_customs_answers_correctly());
 
-    println!("{}", find_my_seat())
+    println!("{}", collate_customs_answers_correctly())
 }
