fca59e3ea46ed4803db2ffc3efa65a71c9dfb672
Tom Pridham <pridham.tom@gmail.com>
2021-12-06 09:59:53 -0700

    feat: day2 2021

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,5 +1,10 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
 mod day1;
-use crate::day1::calculate_sliding_depth;
+mod day2;
+
+use crate::day1::{calculate_depth, calculate_sliding_depth};
+use crate::day2::{calculate_final_course, calculate_final_course_with_aim};
 fn main() {
-    calculate_sliding_depth();
+    calculate_final_course_with_aim();
 }
