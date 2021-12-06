e863e232ca41705d1b05d1bc35d2e5e27935537a
Tom Pridham <pridham.tom@gmail.com>
2021-12-06 15:15:58 -0700

    feat: completed day 3

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -2,9 +2,12 @@
 #![allow(unused_imports)]
 mod day1;
 mod day2;
+mod day3;
 
 use crate::day1::{calculate_depth, calculate_sliding_depth};
 use crate::day2::{calculate_final_course, calculate_final_course_with_aim};
+use crate::day3::{calculate_life_support, calculate_power_consumption};
+
 fn main() {
-    calculate_final_course_with_aim();
+    calculate_life_support();
 }
