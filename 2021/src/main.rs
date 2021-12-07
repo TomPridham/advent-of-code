b6249e82c0156f366a16578aede98ae2ca4cb1f6
Tom Pridham <pridham.tom@gmail.com>
2021-12-07 15:36:41 -0700

    feat: day4 completed finally

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -3,11 +3,13 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 use crate::day1::{calculate_depth, calculate_sliding_depth};
 use crate::day2::{calculate_final_course, calculate_final_course_with_aim};
 use crate::day3::{calculate_life_support, calculate_power_consumption};
+use crate::day4::{find_losing_board, find_winning_board};
 
 fn main() {
-    calculate_life_support();
+    find_losing_board();
 }
