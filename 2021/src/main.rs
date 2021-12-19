03b313a7a526b980dcb3c903b8c107d8c08da917
Tom Pridham <pridham.tom@gmail.com>
2021-12-18 22:20:15 -0700

    feat: finished day six finally

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -5,13 +5,15 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 use crate::day1::{calculate_depth, calculate_sliding_depth};
 use crate::day2::{calculate_final_course, calculate_final_course_with_aim};
 use crate::day3::{calculate_life_support, calculate_power_consumption};
 use crate::day4::{find_losing_board, find_winning_board};
 use crate::day5::{find_danger_vents, find_danger_vents_diag};
+use crate::day6::{model_lanternfish_growth, model_lanternfish_growth_naive};
 
 fn main() {
-    find_danger_vents_diag();
+    model_lanternfish_growth()
 }
