22e8ee6faa926f430d83950c47de33f625d3d789
Tom Pridham <pridham.tom@gmail.com>
2021-12-19 16:14:32 -0700

    feat: finish day7

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(int_abs_diff)]
+#![feature(iter_intersperse)]
 #![allow(dead_code)]
 #![allow(unused_imports)]
 mod day1;
@@ -6,6 +8,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 use crate::day1::{calculate_depth, calculate_sliding_depth};
 use crate::day2::{calculate_final_course, calculate_final_course_with_aim};
@@ -13,7 +16,8 @@ use crate::day3::{calculate_life_support, calculate_power_consumption};
 use crate::day4::{find_losing_board, find_winning_board};
 use crate::day5::{find_danger_vents, find_danger_vents_diag};
 use crate::day6::{model_lanternfish_growth, model_lanternfish_growth_naive};
+use crate::day7::{find_optimal_crab_depth_constant, find_optimal_crab_depth_linear};
 
 fn main() {
-    model_lanternfish_growth()
+    find_optimal_crab_depth_linear();
 }
