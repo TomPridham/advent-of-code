580178e498794b0d14f01bee8830d1f1990f0ac4
Tom Pridham <pridham.tom@gmail.com>
2024-12-03 14:46:35 -0700

    feat: day3

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
 mod day1;
 mod day2;
+mod day3;
 mod read_input;
 
 fn main() {
-    let res = day2::get_dampened_reports();
+    let res = day3::get_enabled_muls();
     println!("{res}");
 }
